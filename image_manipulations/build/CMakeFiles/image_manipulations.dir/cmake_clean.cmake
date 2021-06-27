0cd7888869d58788af623d33cb59ec82bfb0dce3
kirill.saidov <kirill.saidov.d@gmail.com>
2021-06-27 16:25:52 +0600

    initial commit

---
+++ image_manipulations/build/CMakeFiles/image_manipulations.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/KS/Desktop/media/dev/repos/gitlab/cvadventurer/bin/image_manipulations"
+  "/Users/KS/Desktop/media/dev/repos/gitlab/cvadventurer/bin/image_manipulations.pdb"
+  "CMakeFiles/image_manipulations.dir/src/main.cpp.o"
+  "CMakeFiles/image_manipulations.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/image_manipulations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ image_manipulations/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+int main(void) {
+    std::cout << "hello, fdetector!\n";
+}
+
+
+
+
